8568b29e56b5f76fd2359da9b842c24015f9c46c
Granit Haxhaj <granit.haxhaj@hcl.com>
2021-08-25 11:53:15 -0400

    fix on post entity model

--- src/main/java/com/example/demo/model/Post.java
+++ src/main/java/com/example/demo/model/Post.java
@@ -41,6 +41,6 @@ public class Post {
 	private User user;
 	private Instant createdDate;
 	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "id", referencedColumnName = "id")
+	@JoinColumn(name = "id", referencedColumnName = "blogId")
 	private Subreddit subreddit;
 }
